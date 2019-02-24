243387ecb8e45fc2549479d17b4b8527d9099dfc
Aaron W Johnson <aaron.johnson@healthcatalyst.com>
2019-02-23 21:09:55 -0700

    minor tweaks to get things started

--- src/app.js
+++ src/app.js
@@ -1,32 +1,39 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+import Vue from "vue";
+import Vuetify from "vuetify";
 
-import router from './app/router'
-import App from './app/App.vue'
+import router from "./app/router";
+import App from "./app/App.vue";
 
-import './assets/yui-cssreset.css'
+import "./assets/yui-cssreset.css";
 
-import './main.css'
+import "./main.css";
+import "vuetify/dist/vuetify.min.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-      console.log('SW registered: ', registration)
-    }).catch((registrationError) => {
-      console.log('SW registration failed: ', registrationError)
-    })
-  })
+Vue.use(Vuetify);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(registration => {
+        console.log("SW registered: ", registration);
+      })
+      .catch(registrationError => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
 }
 
 /* eslint-disable no-new */
 export default new Vue({
-  el: '#app',
+  el: "#app",
   components: { App },
   router,
   render(h) {
-    return h('App')
-  },
-})
+    return h("App");
+  }
+});
